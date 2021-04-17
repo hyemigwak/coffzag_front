6cfdbfec2db45b4a85a6077c8b9fb3d764a3d868
kyngmn <79073979+kyngmn@users.noreply.github.com>
2021-04-17 19:12:40 +0900

    ID 중복체크
    ID 중복체크

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,8 +17,6 @@ const Product = (props) => {
   } = props;
 
   const reviewList = useSelector((state) => state.product.latest_review);
-  console.log(reviewList);
-  console.log(reviewList[0].contents);
 
   return (
     <Card
